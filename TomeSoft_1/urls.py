c432f892fd3ca91fd260947cce7fbbfae041aa28
Betancourt1996 <braybeta@espol.edu.ec>
2026-07-08 16:21:41 -0500

    pruebas mejor deploy del admin

--- TomeSoft_1/urls.py
+++ TomeSoft_1/urls.py
@@ -1,8 +1,17 @@
+import os
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import include
+from django.http import HttpResponse
+from django.views.static import serve
+
+
+def serve_spa(request, path):
+    if '.' in path.rsplit('/', 1)[-1]:
+        return serve(request, path, document_root=settings.STATIC_ROOT)
+    with open(os.path.join(settings.STATIC_ROOT, 'index.html'), 'rb') as f:
+        return HttpResponse(f.read(), content_type='text/html')
 
 
 urlpatterns = [
@@ -11,8 +20,8 @@ urlpatterns = [
     path('solicitante/', include('core.urls.solicitante')),
     path('web/',         include('core.urls.web')),
     path('admin/', admin.site.urls),
+    re_path(r'^static/(?P<path>.*)$', serve_spa),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root= settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
